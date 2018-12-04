8acb212bfd434c28c7d3cf2d5f1d069c5b3887bd
koti2 <kotireddyaluri@users.noreply.github.com>
2018-12-04 18:44:23 +0530

    Update hmac_hashes.py

--- hmac_hashes.py
+++ hmac_hashes.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-from hashlib import md5,sha1,sha256
+from hashlib import md5,sha1,sha256,sha512
 trans_5C = "".join(chr(x ^ 0x5c) for x in xrange(256))
 trans_36 = "".join(chr(x ^ 0x36) for x in xrange(256))
 md5_blocksize = md5().block_size
 sha1_blocksize = sha1().block_size
 sha256_blocksize = sha256().block_size
+sha512_blocksize = sha512().block_size
 
 print "https://en.wikipedia.org/wiki/HMAC#Implementation"
 
@@ -31,8 +32,16 @@ def hmac_sha256(key,msg):
 	o_key_pad = key.translate(trans_5C)
 	i_key_pad = key.translate(trans_36)
 	return sha256(o_key_pad+sha256(i_key_pad+msg).digest()).hexdigest()
+def hmac_sha512(key,msg):
+	if len(key) > sha512_blocksize:
+		key = sha512(key).digest()
+	key += chr(0)* (sha512_blocksize - len(key))
+	o_key_pad = key.translate(trans_5C)
+	i_key_pad = key.translate(trans_36)
+	return sha512(o_key_pad+sha512(i_key_pad+msg).digest()).hexdigest()
 
 if __name__ == "__main__":
 	print "MD5: "+hmac_md5("key", "The quick brown fox jumps over the lazy dog")
 	print "SHA1: "+hmac_sha1("key", "The quick brown fox jumps over the lazy dog")
 	print "SHA256: "+hmac_sha256("key", "The quick brown fox jumps over the lazy dog")
+	print "SHA512: "+hmac_sha512("key", "The quick brown fox jumps over the lazy dog")
